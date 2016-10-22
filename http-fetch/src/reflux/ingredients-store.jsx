9a8b003273d61fbea6d55903eb05580a5cf16bf7
Arbie C <acskaleidoscope@gmail.com>
2016-10-22 22:31:00 +0900

    build some reflux flow

--- http-fetch/src/reflux/ingredients-store.jsx
+++ http-fetch/src/reflux/ingredients-store.jsx
@@ -12,7 +12,21 @@ var IngredientStore = Reflux.createStore({
         }.bind(this));
     },
     postIngredient: function(text) {
+        if (!this.ingredients) {
+            this.ingredients = [];
+        }
+        var ingredient = {
+            "id": Math.random().toString(36).substr(2, 6),
+            "text": text
+        };
 
+        this.ingredients.push(ingredient);
+        this.fireUpdate();
+
+        HTTP.post('/ingredients', ingredient)
+        .then(function(response) {
+            this.getIngredients();
+        }.bind(this));
     },
     fireUpdate: function() {
         this.trigger('change', this.ingredients);
